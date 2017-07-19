af43438609401c97ba4b84c4d9efde046b2ae630
Matthew Goldwater <mattgwater@gmail.com>
2017-07-19 15:53:24 -0700

    Building a JavaScript Development Environment - 10 - Testing and Continuous Integration - Demo: Testing Setup
    1. setup testSetup.js file for our tests. 2. Set up script in package.json to run tests. 3. setup sample test in index.test.js file

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,9 @@
+
+// this file isn't transpiled so it must use CommonJS and ES5
+
+// Register babel to transpile before our tests run.
+require('babel-register')();
+
+// Disable webpack features that mocha doesn't understand.
+require.extensions['.css'] = function() {};
+
