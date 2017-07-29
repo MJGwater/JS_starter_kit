3dceaf59349d346e2db6c4f69a54d9dc49506fa0
Matthew Goldwater <mattgwater@gmail.com>
2017-07-29 12:09:18 -0700

    Building a JavaScript Development Environment - 14 - Production Deploy - Demo: Automated API Deploy Via Heroku
    change url for the production API now that its hosted on heroku

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://dry-dusk-93112.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
